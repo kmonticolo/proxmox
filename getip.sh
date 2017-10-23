fecee932d8e47d175aba59d26c4d6b4322393480
Kamil Monticolo <kmonticolo@gmail.com>
2017-10-23 11:27:44 +0200

    interface and subnet to vars, arpscan check

--- getip.sh
+++ getip.sh
@@ -2,6 +2,8 @@
 # https://github.com/kmonticolo/proxmox
 
 # get an IP ov VM
+INTERFACE="vmbr0"
+SUBNET="192.168.1.0/24"
 
 NAME=$1
 
@@ -11,24 +13,31 @@ if [ -z $1 ]; then {
 }
 fi
 
-VMID=$(qm list|grep -w $NAME| awk '{print $1}')
+VMID=$(/usr/sbin/qm list|grep -w $NAME| awk '{print $1}')
 if [ -z $VMID ]; then {
   echo "No such vm name"
   exit 2
 }
 fi
 
-MAC=$(qm config $VMID |grep net |sed -e 's/^.*virtio=//' -e 's/,.*$//' | tr [:upper:] [:lower:])
+MAC=$(/usr/sbin/qm config $VMID |grep net |sed -e 's/^.*virtio=//' -e 's/,.*$//' | tr [:upper:] [:lower:])
 if [ -z $MAC ]; then {
   echo "Cannot get MAC address for $NAME"
   exit 3
 }
 fi
 
-IP=$( arp-scan -I vmbr0 192.168.1.0/24|grep $MAC |awk '{ print $1 }')
+ARPSCAN=$(/usr/bin/which arp-scan)
+if [ -z $ARPSCAN ]; then {
+  echo "No arp-scan binary found"
+  exit 4
+}
+fi
+
+IP=$(${ARPSCAN} -I ${INTERFACE} ${SUBNET} |grep $MAC |awk '{ print $1 }')
 if [ -z $IP ]; then {
   echo "Cannot get IP address for $NAME"
-  exit 4
+  exit 5
 }
 fi
 
